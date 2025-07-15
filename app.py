e3a67ef0462184dfff36109ae224809106955fb1
EITANZIADA <ytnzydh1@gmail.com>
2025-07-15 10:32:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def webhook():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5100, debug=True)
